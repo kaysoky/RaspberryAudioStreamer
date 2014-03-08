f4e2da2db84f405d51a6b0272867ff799fad33dd
Joseph Wu <kaysoky88@gmail.com>
2014-03-07 21:15:41 -0800

    Updating server to match new streamer

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import web
 import json
-import streamer
+from streamer import DropboxAudioStreamer
 
 urls = (
     '/', 'generic_getter', 
@@ -19,25 +19,26 @@ class generic_getter:
 
 class current_song:
     def GET(self):
-        return streamer.CurrentSong[0]
+        return web.streamer.CurrentSong
         
 class song_list:
     def GET(self):
         web.header('Content-Type', 'application/json')
         
-        streamer.MusicListLock.acquire()
-        musicList = json.dumps(streamer.MusicList)
-        streamer.MusicListLock.release()
+        web.streamer.MusicListLock.acquire()
+        musicList = json.dumps(web.streamer.MusicList)
+        web.streamer.MusicListLock.release()
         return musicList
         
 class next_song:
     def POST(self, name):
-        streamer.fillMusicBuffer(name)
+        web.streamer.MusicBufferer.addSong(name)
         raise web.seeother('/')
 
 web.internalerror = web.debugerror
 if __name__ == '__main__':
-    streamer.do_main()
+    web.streamer = DropboxAudioStreamer()
+    web.streamer.start()
     
     app = web.application(urls, globals())
     app.run()
